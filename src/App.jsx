df6d790d4a365e9afc652bedfd25b2004dc56ac3
ahmedtika74 <tekaliker@gmail.com>
2026-03-18 20:09:19 +0200

    remove empty app.css, fix spelling in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
@@ -6,7 +5,7 @@ import Cart from "./pages/Cart";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import Categories from "./pages/Categories";
-import CategoryDetailes from "./pages/CategoryDetails";
+import CategoryDetails from "./pages/CategoryDetails";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/layout/Footer";
 
@@ -19,10 +18,7 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/categories" element={<Categories />} />
-        <Route
-          path="/categories/:categoryName"
-          element={<CategoryDetailes />}
-        />
+        <Route path="/categories/:categoryName" element={<CategoryDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
